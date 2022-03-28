0f1486df9659f6d13262ec6839a06094682aacae
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2022-03-28 09:30:42 -0400

    Java Boot Camp: day01

---
+++ src/day01/Primitives.java
@@ -0,0 +1,34 @@
+package day01;
+
+public class Primitives {
+
+    public static void main(String[] args) {
+
+        //DataType var = data
+
+        int  num1 = 100;
+        double num2 = 100; //100.0
+        char ch1 = 16000;
+        boolean r1 =  45 > 30;
+
+        System.out.println(ch1);
+
+        System.out.println("------------------------------");
+
+        byte  b = 100;
+        short s = (short)b;  //implicit casting
+
+        int i = 300;
+        long l = i;   //implicit casting
+
+        System.out.println("------------------------------");
+
+        long n1 = 500;
+        int n2 = (int) n1; // explicit casting
+
+
+
+
+    }
+
+}
