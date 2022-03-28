ce30499470bcc27f4090aa7f3c635b2b5fa02d29
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2022-03-28 14:39:15 -0400

    Java Boot Camp: day01

--- src/day01/Encapsulation.java
+++ src/day01/Encapsulation.java
@@ -5,6 +5,11 @@ class Employee{
     private String name;
     private double salary;
 
+    public Employee(String name, double salary) {
+        setName(name);
+        setSalary(salary);
+    }
+
     public double getSalary() {
         return salary;
     }
@@ -18,6 +23,9 @@ class Employee{
     }
 
     public String getName() {
+        if(name == null){
+            throw new RuntimeException("Name can not be null");
+        }
         return name;
     }
 
@@ -41,13 +49,12 @@ public class Encapsulation {
 
     public static void main(String[] args) {
 
-        Employee employee1 = new Employee();
+        Employee employee1 = new Employee("", -2000);
        // employee1.name = "";
       //  employee1.salary = -100000;
 
-        employee1.setSalary(-100);
-
         System.out.println(employee1);
+
 
 
 
