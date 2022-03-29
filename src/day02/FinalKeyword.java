4b9a689eede05692a057796e5c4462f62fed7fe5
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2022-03-29 09:31:30 -0400

    Java Boot Camp: day02

---
+++ src/day02/FinalKeyword.java
@@ -0,0 +1,27 @@
+package day02;
+
+public class FinalKeyword {
+
+    public static void main(String[] args) {
+
+        final int a = 30;
+       // a = 40;
+       // a = 50;
+
+    }
+
+
+    public final void method(){
+        System.out.println("A");
+    }
+
+
+}
+
+class D extends FinalKeyword{
+   /*
+    public void method(){
+        System.out.println("B");
+    }
+    */
+}
